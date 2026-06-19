e3a94baa4b8111970fb742ae27747347192f7800
c012n <utoki0716@gmail.com>
2026-06-19 14:59:15 +0900

    楽曲のリストからランダム抽選して表示する機能、重複選曲を許すか切り替える機能の実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,122 +1,63 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  // 楽曲の状態
+  const [song, setSong] = useState("（なし）");
+
+  // 選曲済み楽曲リストの状態
+  const [selectedSongs, setSelectedSongs] = useState<string[]>([]);
+
+  // 重複選曲を許すかどうかの状態
+  const [canSelectTwice, setCanSelectTwice] = useState(false);
+
+  // 楽曲群の宣言(とりあえずの動作確認用)
+  const songs = [
+    "Op.I -fear TITΛN-",
+    "MarbleBlue",
+    "Stardust:RAY",
+    "光焔のラテラルアーク",
+    "Selenadia"
+  ];
+
+  // 抽選可能楽曲の状態
+  const availableSongs = canSelectTwice ? songs
+  : songs.filter(song => !selectedSongs.includes(song));
+
+  // 抽選する関数
+  const selectSong = () => {
+    if (availableSongs.length === 0) {
+      setSong("全曲抽選済みです");
+      return;
+    }
+    // ランダムにインデックスを抽選して選曲
+    const randomIndex = Math.floor(Math.random() * availableSongs.length);
+    const selectedSong = availableSongs[randomIndex];
+    setSong(selectedSong);
+    // 抽選済み楽曲リストに追加
+    setSelectedSongs([
+      ...selectedSongs,
+      selectedSong
+    ]);
+  };
 
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
-  )
+  <div>
+    <h1>Otoge Song Randomizer</h1>
+    <label>
+      <input
+        type="checkbox"
+        checked={canSelectTwice}
+        onChange={(e) => setCanSelectTwice(e.target.checked)}
+      />
+      一度出た楽曲も選曲する
+    </label>
+    <p>現在の曲：</p>
+    <p>{song}</p>
+    <button onClick={selectSong}>
+      選曲！
+    </button>
+  </div>
+  );
 }
 
-export default App
+export default App;
