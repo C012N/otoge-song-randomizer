51bf65eb3bf6f1f6cc637d3816d249e52cda570a
C012N <utoki0716@gmail.com>
2026-08-18 11:40:39 +0900

    試合結果表示の修正

--- src/components/ShowRoundResult.tsx
+++ src/components/ShowRoundResult.tsx
@@ -18,6 +18,8 @@ export function ShowRoundResult({
     divisionName,
     roundName,
     roundState,
+    teamNameA,
+    teamNameB,
     playerA,
     playerB,
     scoresPlayerA,
@@ -36,37 +38,61 @@ export function ShowRoundResult({
     
     const formatScore = (score: number) => score.toLocaleString();
 
-    const showPlayerResult = (player: Player, scores: number[], winner: boolean) => {
+    const showPlayerResult = (teamName: string, player: Player, scores: number[], winner: boolean) => {
+        const teamColor = (teamName === teamNameA) ? 'red-500' : 'blue-500';
         const totalScore = scores.reduce((acc, score) => acc + score, 0);
         return (
-            <div className={`w-full rounded-3xl border-3 ${winner ? 'border-green-500' : 'border-gray-600'} bg-slate-900/80 grid grid-cols-3 items-center text-white text-center mt-6 p-4`}>
-                <div className="font-bold text-4xl">
-                    {player.name}
+            <div className={`
+                w-full rounded-3xl border-3
+                ${winner ? 'border-green-500' : 'border-gray-600'}
+                bg-slate-900/70 flex items-center justify-around
+                text-white text-center py-6`}>
+                
+                <div className="flex gap-4 items-center font-bold">
+                    <p className={`rounded-full bg-${teamColor} text-black text-2xl px-3 py-1`}>
+                        {teamName}
+                    </p>
+                    <p className="text-4xl">
+                        {player.name}
+                    </p>
                 </div>
-                <div className="font-bold text-6xl">
+
+                <p className="font-bold text-6xl">
                     {formatScore(totalScore)}
-                </div>
-                <div className="text-2xl">
+                </p>
+                <p className="text-2xl">
                     ①{formatScore(scores[0])} <br />
                     ②{formatScore(scores[1])} <br />
                     ③{formatScore(scores[2])}
-                </div>
+                </p>
             </div>
         );
     }
 
     return (
-        <div className="mx-auto px-4 py-4 text-center md:px-10 md:py-6">
-            <h1 className="[text-shadow:_2px_2px_0_rgb(64_64_64),_-2px_2px_0_rgb(64_64_64),_2px_-2px_0_rgb(64_64_64),_-2px_-2px_0_rgb(64_64_64)]">
+        <div className="max-w-7xl mx-auto
+        flex flex-col gap-12">
+            <p className="
+            text-6xl font-serif pt-15
+            [text-shadow:_0_0_10px_purple,_2px_2px_2px_black]
+            [text-shadow:]">
                 {divisionName}部門 {roundName}
-            </h1>
-            <p className="py-2 text-xl text-white text-left">
-                ① {playerA.song.title} / ② {playerB.song.title} / ③ {roundState.selectedSong.title}
             </p>
+            <div className="
+            [text-shadow:_0_0_4px_purple]
+            text-3xl text-white
+            flex flex-col items-center">
+                <div className="text-left">
+                    <p>① {playerA.song.title}</p>
+                    <p>② {playerB.song.title}</p>
+                    <p>③ {roundState.selectedSong.title}</p>
+                </div>
+                
+            </div>
 
-            {showPlayerResult(playerA, scoresPlayerA, totalScoreA >= totalScoreB)}
+            {showPlayerResult(teamNameA, playerA, scoresPlayerA, totalScoreA >= totalScoreB)}
 
-            {showPlayerResult(playerB, scoresPlayerB, totalScoreB >= totalScoreA)}
+            {showPlayerResult(teamNameB, playerB, scoresPlayerB, totalScoreB >= totalScoreA)}
         </div>
     )
 }
