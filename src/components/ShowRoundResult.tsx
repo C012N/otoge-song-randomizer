a607a77934db2c249b7871164be97176ccbb141f
C012N <utoki0716@gmail.com>
2026-08-26 10:18:30 +0900

    試合結果表示画面のズレを修正

--- src/components/ShowRoundResult.tsx
+++ src/components/ShowRoundResult.tsx
@@ -64,8 +64,8 @@ export function ShowRoundResult({
             <div className={`
                 w-full rounded-3xl border-3
                 ${winner ? 'border-green-500' : 'border-gray-600'}
-                bg-slate-900/70 flex items-center justify-around
-                text-white text-center py-6`}>
+                bg-slate-900/70 grid grid-cols-3 items-center justify-items-start
+                text-white text-center px-8 py-6`}>
                 
                 <div className="flex gap-4 items-center font-bold">
                     <p className={`rounded-full ${teamColor} text-black text-2xl px-3 py-1`}>
